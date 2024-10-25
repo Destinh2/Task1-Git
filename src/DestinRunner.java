299d85d396613f70e2fb9df9624e0a5d389fa145
BT_1N3_08 <sample@bths.edu>
2024-10-25 10:03:18 -0400

    Added scanner and some more print statements

--- src/DestinRunner.java
+++ src/DestinRunner.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
 public class DestinRunner {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         System.out.println("hello fellow people");
+        System.out.println("Hello my name is destin what is urs?");
+        String personName = scan.nextLine();
+        System.out.println(personName);
     }
 }
